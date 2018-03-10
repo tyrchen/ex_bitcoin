8ab18458e9f48e6330c0ae24c91fd422b1f6d042
Tyr Chen <tyr.chen@gmail.com>
2018-03-09 21:15:19 -0800

    add TODO

--- lib/utility/utils.ex
+++ lib/utility/utils.ex
@@ -6,10 +6,12 @@ defmodule ExBitcoinUtils do
   end
 
   def gen_spec(args) do
-    Enum.map(args, fn _ -> String.to_atom("Elixir.String.t") end)
+    # TODO: this is fake now
+    Enum.map(args, fn _ -> String.to_atom("String.t") end)
   end
 
   def gen_args(args, ctx \\ nil) do
+    # TODO: generate default values
     Enum.map(args, fn %{"name" => name} -> Macro.var(String.to_atom(name), ctx) end)
   end
 
